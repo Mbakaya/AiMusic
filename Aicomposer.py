10f6f43c6de94c6a58f124781d31267305e744fd
Mbakaya Weever <officialmbakaya@gmail.com>
2017-10-11 14:14:50 +0300

    Update Aicomposer.py

--- Aicomposer.py
+++ Aicomposer.py
@@ -3,12 +3,14 @@ import zipfile
 import nottingham_util
 import rnn
 
+#finding our dataset zip using urllib from the given url
 url = "www-etud.iro.umontreal.ca/~boulanni/Nottingham.zip"
 urllib.urlretrieve(url,"dataset.zip")
 
+#extract the zipfile to gt the data
 zip = zipfile.ZipFile(r'dataset.zip')
 zip.extractall('data')
 
 nottingham_util.create_model()
-
+#call train model method  of neural net class
 rnn.train_model()
